97c3fad0f6a7756d795b7810749ae5af5c482a1c
재현 박 <kat3160@naver.com>
2020-02-17 16:08:05 +0900

    Java Eclipse Programmers test

---
+++ src/Hash1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/*
+ Question's URL: https://programmers.co.kr/learn/courses/30/lessons/42576
+*/
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for(int i=0;i<completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                answer = participant[i];
+                break;
+            } else {
+                answer = participant[i+1];
+            }
+        }
+        return answer;
+    }
+}
